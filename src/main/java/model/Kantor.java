eaeea277a3b95b3bf7aa04fd70b5588284a156cb
Surya Aulia <120304394+SuryaaAulia@users.noreply.github.com>
2024-06-13 20:54:25 +0700

    Update Kantor.java

--- src/main/java/model/Kantor.java
+++ src/main/java/model/Kantor.java
@@ -8,64 +8,64 @@ import java.util.stream.Collectors;
 
 public class Kantor {
 
-    private String nama;
-    private List<Pegawai> daftarPegawai;
+    private String name;
+    private List<Pegawai> employeeList;
 
     public Kantor(String n) {
         if (n == null) {
             throw new IllegalArgumentException("Company name cannot be null");
         }
-        nama = n;
-        daftarPegawai = new ArrayList<>();
+        name = n;
+        employeeList = new ArrayList<>();
     }
 
-    public List<Pegawai> getDaftarPegawai() {
-        if (daftarPegawai == null) {
+    public List<Pegawai> getEmployeeList() {
+        if (employeeList == null) {
             return new ArrayList<>();
         }
-        return daftarPegawai;
+        return employeeList;
     }
 
-    public void tambahPegawai(Pegawai peg) {
+    public void addEmployee(Pegawai peg) {
         if (peg != null) {
-            daftarPegawai.add(peg);
+            employeeList.add(peg);
         } else {
-            daftarPegawai.add(null);
+            employeeList.add(null);
         }
     }
 
-    public void hapusPegawai(String namaPeg) {
-        for (int i = 0; i < daftarPegawai.size(); i++) {
-            if (daftarPegawai.get(i).getNama().equals(namaPeg)) {
-                daftarPegawai.remove(i);
+    public void removeEmployee(String employeeName) {
+        for (int i = 0; i < employeeList.size(); i++) {
+            if (employeeList.get(i).getName().equals(employeeName)) {
+                employeeList.remove(i);
                 i--;
             }
         }
     }
 
-    public void urutkanSesuaiGaji() {
-        Collections.sort(daftarPegawai);
-        Collections.sort(daftarPegawai);
+    public void sortBySalary() {
+        Collections.sort(employeeList);
+        Collections.sort(employeeList);
     }
 
-    public void urutkanSesuaiNama() {
-        Collections.sort(daftarPegawai, new Comparator<Pegawai>() {
+    public void sortByName() {
+        Collections.sort(employeeList, new Comparator<Pegawai>() {
             @Override
             public int compare(Pegawai o1, Pegawai o2) {
-                return o1.getNama().compareTo(o2.getNama());
+                return o1.getName().compareTo(o2.getName());
             }
         });
-        Collections.sort(daftarPegawai, new Comparator<Pegawai>() {
+        Collections.sort(employeeList, new Comparator<Pegawai>() {
             @Override
             public int compare(Pegawai o1, Pegawai o2) {
-                return o1.getNama().compareTo(o2.getNama());
+                return o1.getName().compareTo(o2.getName());
             }
         });
     }
 
-    public ArrayList<Pegawai> getSesuaiUmur(int umur) {
-        return daftarPegawai.stream()
-                .filter(Pegawai -> Pegawai.getUmur() == umur)
+    public ArrayList<Pegawai> filterByAge(int age) {
+        return employeeList.stream()
+                .filter(Pegawai -> Pegawai.getAge() == age)
                 .collect(Collectors.toCollection(ArrayList::new));
     }
 }
